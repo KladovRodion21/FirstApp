888c367fb6d0c9a786f87190bf75f491cb27d802
kladov <kladov_1999@inbox.ru>
2021-10-12 10:18:28 +0500

    hhh

---
+++ app/src/main/java/com/example/firstapp/recadapters/Feel.kt
@@ -0,0 +1,15 @@
+package com.example.firstapp.recadapters
+import com.example.firstapp.R
+
+data class Feel(val image:Int, val name_feel:String){
+    class MyFeel{val list = arrayListOf(
+        Feel(R.drawable.elementburger, "Бургер"),
+        Feel(R.drawable.elementnug, "Нагетсы"),
+        Feel(R.drawable.elementkril, "Крылышки"),
+        Feel(R.drawable.elementrol, "Роллы"),
+                Feel(R.drawable.elementburger, "Бургер"),
+        Feel(R.drawable.elementnug, "Нагетсы"),
+        Feel(R.drawable.elementkril, "Крылышки"),
+        Feel(R.drawable.elementrol, "Роллы")
+    )}
+}
